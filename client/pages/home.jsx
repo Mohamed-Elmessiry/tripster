b2b201620c3eaca73d32a8d890b9c1acb4421216
Mohamed Elmessiry <mohamedelmessiry@hotmail.com>
2021-05-27 21:19:34 +0000

    completed styling and rendering data

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,8 +1,91 @@
+
 import React from 'react';
 
-export default function Home(props) {
-  return (
-    <>
-    </>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { searchtext: '', venues: [], activity: '' };
+    this.text = '';
+    this.handlechange = this.handlechange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handlechange(event) {
+    const value = event.target.value;
+    const name = event.target.name;
+    this.setState({ [name]: value });
+
+  }
+
+  formatData(venues) {
+    return venues.map(({ location, name, categories }) => {
+      const { address } = location;
+      const { pluralName, icon: { prefix } } = categories[0];
+      return {
+        address,
+        name,
+        prefix,
+        pluralName
+      };
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const activity = this.state.activity;
+    const location = this.state.searchtext;
+    const api = `http://localhost:3001/api/venues/${location}/${activity}`;
+    fetch(api)
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        // eslint-disable-next-line no-debugger
+        // eslint-disable-next-line no-console
+        console.log(data);
+        // eslint-disable-next-line no-console
+
+        this.setState({ venues: this.formatData(data) });
+      })
+      .catch(error => {
+        // eslint-disable-next-line no-console
+        console.log('error', error);
+
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="header">
+          <div className="buttons-holder">
+            <button className="main-page">Main Page</button>
+            <button className="favorites">Favorites</button>
+          </div>
+
+        </div>
+        <h1 className= "tripster-header">Tripster</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.searchtext} name="searchtext" placeholder="City" onChange={this.handlechange} />
+          <input type="text" value={this.state.activity} name="activity" placeholder="Acitivity" onChange={this.handlechange} />
+          <button type="submit">Search</button>
+        </form>
+        {
+          this.state.venues.map((venue, i) => (
+            <div className="venue-card" key={`venue-${i}`}>
+              <img src={`${venue.prefix}.png`} alt="location image"/>
+              <div>
+                <h3>{venue.name}</h3>
+                <p>{venue.pluralName}</p>
+                <p>{venue.address}</p>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+    );
+  }
+
 }
+
+export default App;
--- server/index.js
+++ server/index.js
@@ -4,9 +4,26 @@ const errorMiddleware = require('./error-middleware');
 const staticMiddleware = require('./static-middleware');
 const fetch = require('node-fetch');
 const ClientError = require('./client-error');
+const cors = require('cors');
 // const { query } = require('express');
 
 const app = express();
+app.use(cors());
+
+// eslint-disable-next-line no-unused-vars
+const formatData = json => {
+  return json.response.venues
+    .map(({ location, name, categories }) => {
+      const { address } = location;
+      const { pluralName, icon: { prefix } } = categories[0];
+      return {
+        address,
+        name,
+        prefix,
+        pluralName
+      };
+    });
+};
 
 app.use(staticMiddleware);
 
@@ -20,8 +37,14 @@ app.get('/api/venues/:city?/:search?', (req, res, next) => {
   fetch(
     `https://api.foursquare.com/v2/venues/search?client_id=EZOAI304JHK222F243HNN4KULBKUGKG10DPYVDWCHNJWB3HT&client_secret=IG3AUD5AEZ1TVO5PMT43BYTL2WRTIRSKWBUFQ4HZAVTZJYI3&v=20210514&near=${cityname}&intent=browse&radius=10000&query=${searchQuery}&limit=10`
   )
-    .then(res => res.json()) // this is fetch res
-    .then(results => res.json(results)); // this is express res
+    .then(response => {
+      return response.json();
+    }) // this is fetch res
+    .then(data => {
+      res.status(200).json(data.response.venues);
+    });
+
+  // .then(results => res.json(results)); // this is express res
 });
 
 app.use(errorMiddleware);
