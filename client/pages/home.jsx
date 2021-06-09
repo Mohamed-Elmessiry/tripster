37104a980d77f6942bd481156115f7c423dbdb01
Mohamed-Elmessiry <mohamedelmessiry@hotmail.com>
2021-06-09 10:35:15 -0400

    completed feature use can save favorites

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -7,12 +7,12 @@ const App = () => {
   const [searchCity, setSearchCity] = useState('');
   const [imageLinks, setImageLinks] = useState([]);
   const [title, setTitle] = useState('Results');
-  const [imageFlag, setImageFlag] = useState(true);
+  const [imageFlag, setImageFlag] = useState(false);
   // alert('start');
 
   useEffect(() => {
     if (imageFlag) { getImages(); }
-
+    if (localStorage.getItem('favorites')) { favorites(); }
   }, [venues]);
 
   const getImages = e => {
@@ -52,6 +52,7 @@ const App = () => {
   };
   const favorites = e => {
     setImageFlag(false);
+    // alert('favorites');
     const uri = '/api/user/favorites';
     setTitle('Favorites');
     fetch(uri).then(res => {
@@ -68,7 +69,7 @@ const App = () => {
     e.preventDefault();
     setVenues([]);
   };
-  if (localStorage.getItem('favorites')) { favorites(); }
+  // if (localStorage.getItem('favorites')) { favorites(); }
 
   return (
     <div className={venues.length > 0 ? 'app-white' : 'app'}>
